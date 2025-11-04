a9133239cc8db22a794c94f466e548a50db900f6
Richard Stoiberer <rstoiberer@rollins.edu>
2025-11-03 21:25:48 -0500

    Audio fix
    Audio plays now during puzzle game

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,20 +2,25 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    [SerializeField] AudioSource musicSource;
-    [SerializeField] AudioSource sfxSource;
-    public AudioClip backgroundMusic;
-    public AudioClip stumbleSound;
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource sfxSource;
+    [SerializeField] private AudioClip backgroundMusic;
 
-    private void Start()
+    void Start()
     {
-        musicSource.clip = backgroundMusic;
-        musicSource.Play();
+        if (musicSource && backgroundMusic)
+        {
+            musicSource.clip = backgroundMusic;
+            musicSource.loop = true;
+            musicSource.playOnAwake = false; // we’ll call Play() ourselves
+            musicSource.volume = 1f;
+            musicSource.spatialBlend = 0f;   // 2D
+            musicSource.Play();
+        }
     }
 
-    public void PlaySXF(AudioClip clip)
+    public void PlaySfx(AudioClip clip, float vol = 1f)
     {
-        sfxSource.PlayOneShot(clip);
+        if (sfxSource && clip) sfxSource.PlayOneShot(clip, vol);
     }
-    
 }
--- Assets/Scripts/playerMove.cs
+++ Assets/Scripts/playerMove.cs
@@ -2,40 +2,31 @@ using UnityEngine;
 
 public class playerMove : MonoBehaviour
 {
-    Rigidbody2D pad;
-    Vector2 initial;
+    private Rigidbody2D pad;
+    private Vector2 initial;
     public float displacement;
 
     public Animator animator;
 
-    AudioManager audioManager;
-
-    
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         pad = GetComponent<Rigidbody2D>();
         initial = pad.transform.localPosition;
-        audioManager = GameObject.FindGameObjectWithTag("music").GetComponent<AudioManager>();
-       
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if ((Input.GetKey(KeyCode.RightArrow)))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-
             animator.SetBool("isRunning", true);
-            if (initial.x <= 9.75)
-                initial.x = initial.x + displacement;
+            if (initial.x <= 9.75f)
+                initial.x += displacement;
         }
-        else if ((Input.GetKey(KeyCode.LeftArrow)))
+        else if (Input.GetKey(KeyCode.LeftArrow))
         {
-            if (initial.x > -9.75)
-                initial.x = initial.x - displacement;
+            animator.SetBool("isRunning", true);
+            if (initial.x > -9.75f)
+                initial.x -= displacement;
         }
         else
         {
@@ -44,14 +35,4 @@ public class playerMove : MonoBehaviour
 
         pad.MovePosition(initial);
     }
-    
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("brick"))
-        {   Debug.Log("Stumble Sound");
-            audioManager.PlaySXF(audioManager.stumbleSound);
-        }
-        
-    }
-
 }
